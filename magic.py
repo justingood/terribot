d260d3c05a484cadb252054a4599867bcb010a27
Jeff Murchison <jeff@jeffmurchison.com>
2014-04-01 16:00:05 -0400

    Added link title discovery
    Requires BeaufitulSoup library.

--- magic.py
+++ magic.py
@@ -7,6 +7,8 @@ A terrible Telegram bot. NOT Three-Law compatible.
 import sys
 import re
 import pytg
+import urllib2
+
 from pytg.utils import coroutine, broadcast
 from pytg.tg import (
     dialog_list, chat_info, message, user_status,
@@ -17,5 +19,9 @@ def do(msg):
   print ""
   if re.match('ping' ,msg['message']) is not None:
     return 'pong'
+  elif re.search('http' ,msg['message']) is not None:
+      from bs4 import BeautifulSoup
+      soup = BeautifulSoup(urllib2.urlopen(msg['message']))
+      return soup.title.string
   else:
-    return 'what?'
+    return ''
