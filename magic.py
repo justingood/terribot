b3e14437a539be92c3efee648c9f2be49cb67ad9
Jeff Murchison <jeff@jeffmurchison.com>
2014-04-01 19:10:51 -0400

    Changes to YouTube title lookup
    Only returns titles for youtube links now, without “ - YouTube” on the
    end.

--- magic.py
+++ magic.py
@@ -15,13 +15,16 @@ from pytg.tg import (
     )
 
 def do(msg):
-  print msg
-  print ""
-  if re.match('ping' ,msg['message']) is not None:
-    return 'pong'
-  elif re.search('http' ,msg['message']) is not None:
-      from bs4 import BeautifulSoup
-      soup = BeautifulSoup(urllib2.urlopen(msg['message']))
-      return soup.title.string
-  else:
-    return ''
+    print msg
+    print ""
+    # Ping
+    if re.match('ping' ,msg['message']) is not None:
+        return 'pong'
+    # Youtube Title Lookup
+    elif re.search('youtube.com' ,msg['message']) or re.search('youtu.be' ,msg['message']) is not None:
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(urllib2.urlopen(msg['message']))
+        return soup.title.string.replace(" - YouTube","")
+    # Ignore everything else
+    else:
+        return ''
