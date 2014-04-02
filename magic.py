0d1749822229551d4554eb6d1b50018b1894fc32
Justin Good <justin@500px.com>
2014-04-02 10:10:50 -0400

    Varied ping responses

--- magic.py
+++ magic.py
@@ -12,6 +12,7 @@ import random
 from bs4 import BeautifulSoup
 
 colinChoice = ['Who\'s this Colin person you guys keep talking about?', 'Colin? Who\'s that?', 'What\'s a Colin?', 'You guys keep saying that name...', 'I have no idea who you\'re talking about.', 'Stop making up imaginary poeple.', 'This Colin guy sounds as imaginary as human free will', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
+pingChoice = ['I\'m getting tired of ponging', 'Stop it', 'pong', 'pong', 'pong', 'Do you find this amusing?', 'pong', 'Pong', 'Qbert', 'pong', 'Not right now, I\'ve got a headache.', 'pong', 'pong', 'pong', 'pong', 'pong', 'pong', 'pong']
 
 from pytg.utils import coroutine, broadcast
 from pytg.tg import (
@@ -23,7 +24,7 @@ def do(msg):
     print ""
     # Ping
     if re.match('ping' ,msg['message']) is not None:
-        return 'pong'
+        return random.choice(pingChoice)
     # URL Title Lookup
     elif re.search("(?P<url>https?://[^\s]+)", msg['message']) is not None:
         match = re.search("(?P<url>https?://[^\s]+)", msg['message'])
