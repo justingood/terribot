473253b3074fbeca92f3e814bf81630f132b5d33
Jeff Murchison <jeff@jeffmurchison.com>
2014-04-04 14:59:03 -0400

    Dictionary lookups can now be called with define

--- magic.py
+++ magic.py
@@ -15,8 +15,8 @@ from bs4 import BeautifulSoup
 from httplib2 import Http
 from datetime import datetime, timedelta
 
-colinChoice = ['Who\'s this Colin person you guys keep talking about?', 'Colin? Who\'s that?', 'What\'s a Colin?', 'You guys keep saying that name...', 'I have no idea who you\'re talking about.', 'Stop making up imaginary poeple.', 'This Colin guy sounds as imaginary as human free will', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
-pingChoice = ['I\'m getting tired of ponging', 'Stop it', 'pong', 'pong', 'pong', 'Do you find this amusing?', 'pong', 'Pong', 'Qbert', 'pong', 'Not right now, I\'ve got a headache.', 'pong', 'pong', 'pong', 'pong', 'pong', 'pong', 'pong']
+colinChoice = ['Jub\'f guvf Pbyva crefba lbh thlf xrrc gnyxvat nobhg?', 'Pbyva? Jub\'f gung?', 'Jung\'f n Pbyva?', 'Lbh thlf xrrc fnlvat gung anzr...', 'V unir ab vqrn jub lbh\'er gnyxvat nobhg.', 'Fgbc znxvat hc vzntvanel cbrcyr.', 'Guvf Pbyva thl fbhaqf nf vzntvanel nf uhzna serr jvyy', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
+pingChoice = ['V\'z trggvat gverq bs cbatvat', 'Fgbc vg', 'cbat', 'cbat', 'cbat', 'Qb lbh svaq guvf nzhfvat?', 'cbat', 'Cbat', 'Doreg', 'cbat', 'Abg evtug abj, V\'ir tbg n urnqnpur.', 'cbat', 'cbat', 'cbat', 'cbat', 'cbat', 'cbat', 'cbat']
 
 from pytg.utils import coroutine, broadcast
 from pytg.tg import (
@@ -30,7 +30,7 @@ def do(msg):
     terribot.mydelta = timedelta(seconds=30)
     # Ping
     if re.match('ping' ,msg['message']) is not None:
-        return random.choice(pingChoice)
+        return (random.choice(pingChoice)).decode('rot13')
     # URL Title Lookup
     elif re.search("(?P<url>https?://[^\s]+)", msg['message']) is not None:
         match = re.search("(?P<url>https?://[^\s]+)", msg['message'])
@@ -48,32 +48,34 @@ def do(msg):
         return str(msg['message'] + "? " + "I can't give you those, unfortunately.")
     # Colin
     elif re.search('colin' ,msg['message'], re.IGNORECASE) is not None:
-        return random.choice(colinChoice)
+        return (random.choice(colinChoice)).decode('rot13')
     # Urban Dictionary definitions
-    elif (re.search('define:' ,msg['message'], re.IGNORECASE) is not None and len(msg['message'].split()) >1):  #if "define:" is in the message AND message is more than one word.
-        now = datetime.now()
-        if not terribot.last_def or (now - terribot.last_def) >= terribot.mydelta:
-            terribot.last_def = now
-            print terribot.last_def
-            defkeyword = msg['message'].lower()                                 #convert message to lower case
-            h = Http()
-            resp, rawcontent = h.request("http://api.urbandictionary.com/v0/define?term=%s" % urllib2.quote(defkeyword.replace("define:","")), "GET")   #send message to the API, without define keyword
-            if re.search('no_results', rawcontent) is None:                     #if there is a definition for that word
-                rawcontent = rawcontent.replace("\\r", " ").replace("\\n", " ") #remove newline and carriage returns
-                content = json.loads(rawcontent)
-                for item in content['list'][0:1]:
-                    definition = item['definition']                             #populate some variables
-                    permalink = item['permalink']
-                    word = item['word']
-                    example = item['example']
-                    if example:                                                 #if the definition also has an example then show it
-                        return (word + ": " + definition + ".          " + "EXAMPLE: " + example).encode('utf-8', 'replace')
-                    else:
-                        return (word + ": " + definition).encode('utf-8', 'replace')
+    elif (re.search('define' ,msg['message'], re.IGNORECASE) is not None and len(msg['message'].split()) >1):  #if "define" is in the message AND message is more than one word.
+        defkeyword = str(msg['message']).split(' ', 1)[0]
+        if re.search('define', defkeyword):                                         #if define is the FIRST word, otherwise ignore it (so people can still use "define" in a sentence)
+            now = datetime.now()
+            if not terribot.last_def or (now - terribot.last_def) >= terribot.mydelta:
+                terribot.last_def = now                                           
+                h = Http()
+                resp, rawcontent = h.request("http://api.urbandictionary.com/v0/define?term=%s" % urllib2.quote(msg['message'].replace(defkeyword,"")), "GET")   #send message to the API, without define keyword
+                if re.search('no_results', rawcontent) is None:                     #if there is a definition for that word
+                    rawcontent = rawcontent.replace("\\r", " ").replace("\\n", " ") #remove newline and carriage returns
+                    content = json.loads(rawcontent)
+                    for item in content['list'][0:1]:
+                        definition = item['definition']                             #populate some variables
+                        permalink = item['permalink']
+                        word = item['word']
+                        example = item['example']
+                        if example:                                                 #if the definition also has an example then show it
+                            return (word + ": " + definition + ".          " + "EXAMPLE: " + example).encode('utf-8', 'replace')
+                        else:
+                            return (word + ": " + definition).encode('utf-8', 'replace')
+                else:
+                    return "Sorry, but I couldn't find a definition for that word."
             else:
-                return "Sorry, but I couldn't find a definition for that word."
+                return "Sorry, you'll have to wait ~10 seconds to look up another definition."
         else:
-            return "Sorry, you'll have to wait ~10 seconds to look up another definition."
+                return ''
 
     # Ignore everything else
     else:
