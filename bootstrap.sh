ccbc5ac05c545a3133b56a53aec121226d57b87f
Justin Good <justingood@gmail.com>
2014-10-24 10:32:43 +0100

    starts up and kind of gets vm going

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 #do some stuff
+
+apt-get install -y python-pip python-virtualenv libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev git libevent-dev make
+
+sudo -u vagrant git clone https://github.com/efaisal/tg.git /home/vagrant/tg
+cd /home/vagrant/tg
+sudo -u vagrant ./configure && make
+sudo -u vagrant virtualenv /home/vagrant/virtualenv
+
