c6c2b7f580731a2d5ac720591b5fc5e3b44d2f02
billybobmac <billybobmac@gmail.com>
2016-07-13 17:08:16 -0400

    Fixes applied
    Applied above stated fixes. Not able to test locally on work machine. Won't be able to do testing this week.

--- plugins/beertypes.py
+++ plugins/beertypes.py
@@ -1,60 +1,60 @@
 import random
 
 beer_types= ['Altbier',
-			'Düsseldorf Altbier',
-			'Amber ale',
-			'Barley wine',
-			'American Barleywine',
-			'Berliner weisse',
-			'Bière de Garde',
-			'Bitter',
-			'Blonde Ale',
-			'Bock',
-			'Brown ale',
-			'American Brown Ale',
-			'California Common',
-			'Cream Ale',
-			'Dortmunder Export',
-			'Doppelbock',
-			'Dunkel',
-			'Dunkelweizen',
-			'Eisbock',
-			'Flanders red ale',
-			'Golden Ale',
-			'Gose',
-			'Gueuze',
-			'Hefeweizen',
-			'Helles',
-			'India pale ale',
-			'American IPA',
-			'Imperial IPA',
-			'Kölsch',
-			'Lambic',
-			'Light ale',
-			'Maibock',
-			'Malt liquor',
-			'Mild',
-			'Oktoberfestbier',
-			'Old ale',
-			'Oud bruin',
-			'Pale ale',
-			'Pilsener',
-			'German-Style Pilsener',
-			'Bohemian Pilsener',
-			'Classic American Pilsner',
-			'Porter',
-			'Red ale',
-			'Roggenbier',
-			'Saison',
-			'Scotch ale',
-			'Peated Scotch Ale',
-			'Stout',
-			'Oatmeal Stout',
-			'Schwarzbier',
-			'Vienna lager',
-			'Witbier',
-			'Weissbier',
-			'Weizenbock']
+            'Düsseldorf Altbier',
+            'Amber ale',
+            'Barley wine',
+            'American Barleywine',
+            'Berliner weisse',
+            'Bière de Garde',
+            'Bitter',
+            'Blonde Ale',
+            'Bock',
+            'Brown ale',
+            'American Brown Ale',
+            'California Common',
+            'Cream Ale',
+            'Dortmunder Export',
+            'Doppelbock',
+            'Dunkel',
+            'Dunkelweizen',
+            'Eisbock',
+            'Flanders red ale',
+            'Golden Ale',
+            'Gose',
+            'Gueuze',
+            'Hefeweizen',
+            'Helles',
+            'India pale ale',
+            'American IPA',
+            'Imperial IPA',
+            'Kölsch',
+            'Lambic',
+            'Light ale',
+            'Maibock',
+            'Malt liquor',
+            'Mild',
+            'Oktoberfestbier',
+            'Old ale',
+            'Oud bruin',
+            'Pale ale',
+            'Pilsener',
+            'German-Style Pilsener',
+            'Bohemian Pilsener',
+            'Classic American Pilsner',
+            'Porter',
+            'Red ale',
+            'Roggenbier',
+            'Saison',
+            'Scotch ale',
+            'Peated Scotch Ale',
+            'Stout',
+            'Oatmeal Stout',
+            'Schwarzbier',
+            'Vienna lager',
+            'Witbier',
+            'Weissbier',
+            'Weizenbock']
 
 # We'll pad this with extra empty data so we only sporadically comment
 while len(beer_types) < 200:
@@ -66,13 +66,12 @@ def setup():
 
 
 def run(msg):
-	#Chooses beer type
-	beer_type=(random.choice(beer_types))
-	#If beer type is blank option (sporadic comment logic)
-	if beer_type == '':
-		return ()
-	else:
-		#Responds with message plus beer name type
-		answer = msg+' '+ beer_type
-
+    #Chooses beer type
+    beer_type = (random.choice(beer_types))
+    #If beer type is blank option (sporadic comment logic)
+    if beer_type == '':
+        return ()
+    else:
+        #Responds with message plus beer name type
+        answer = msg + ' ' + beer_type
     return ({'action': 'send_msg', 'payload': answer},)
