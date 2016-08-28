78a1fc83086db9f03f5f267d2172347f80e52ba9
Justin Good <justingood@gmail.com>
2016-08-28 19:11:31 -0300

    Add Morbotron support

---
+++ plugins/futurama.py
@@ -0,0 +1,34 @@
+""" Searches for results on Morbotron via the cghmc helper """
+
+import re
+from helpers import cghmc
+
+
+def setup():
+    """ Registers frinkiac plugin. """
+    return {'regex': '(^futurama|^futurama animated)\:', 'act_on_event': 'message'}
+
+
+def run(msg):
+    """ Returns an image from the frinkiac search engine. """
+    message = re.match('(^futurama|^futurama animated)\:(.*) ([0-9]*)', msg['text'], re.IGNORECASE)
+    searchtype = message.group(1)
+    searchterm = message.group(2)
+    result_num = message.group(3)
+
+    # Get search results from cghmc helper
+    if searchtype.lower() == 'futurama':
+        morbo_result = cghmc.search('morbotron', searchterm, result_num, False)
+    elif searchtype.lower() == 'futurama animated':
+        morbo_result = cghmc.search('morbotron', searchterm, result_num, True)
+    else:
+        print("Puny Earthling, you shouldn't be here!")
+
+    # Initialize the first return value tuple with the image itself - we'll add the subtitles after
+    results = ({'action': 'send_photo', 'payload': morbo_result['image']},)
+
+    for subtitle in morbo_result['captions'].json()['Subtitles']:
+        results = results + ({'action': 'send_msg', 'payload': subtitle['Content']},)
+
+    # Return our image and the associated captions
+    return results
--- plugins/simpsons.py
+++ plugins/simpsons.py
@@ -6,21 +6,28 @@ from helpers import cghmc
 
 def setup():
     """ Registers frinkiac plugin. """
-    return {'regex': "^simpsons\:.*", 'act_on_event': 'message'}
+    return {'regex': '(^simpsons|^simpsons animated)\:', 'act_on_event': 'message'}
 
 
 def run(msg):
     """ Returns an image from the frinkiac search engine. """
-    message = re.match('^(simpsons\:) (.*)', msg['text'], re.IGNORECASE)
+    message = re.match('(^simpsons||^simpsons animated)\:(.*)  ([0-9]*)', msg['text'], re.IGNORECASE)
+    searchtype = message.group(1)
     searchterm = message.group(2)
+    result_num = message.group(3)
 
     # Get search results from cghmc helper
-    frinkiac = cghmc.frinkiac(searchterm)
+    if searchtype.lower() == 'simpsons':
+        cghmc_result = cghmc.search('frinkiac', searchterm, result_num, False)
+    elif searchtype.lower() == 'simpsons anmiated':
+        cghmc_result = cghmc.search('frinkiac', searchterm, result_num, True)
+    else:
+        print("Error, you shouldn't be here.")
 
     # Initialize the first return value tuple with the image itself - we'll add the subtitles after
-    results = ({'action': 'send_photo', 'payload': frinkiac['image']},)
+    results = ({'action': 'send_photo', 'payload': cghmc_result['image']},)
 
-    for subtitle in frinkiac['captions'].json()['Subtitles']:
+    for subtitle in cghmc_result['captions'].json()['Subtitles']:
         results = results + ({'action': 'send_msg', 'payload': subtitle['Content']},)
 
     # Return our image and the associated captions
